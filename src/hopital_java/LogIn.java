2924b095b17eb6759aa5b7daadf77ac8a9c56914
tvng <37909639+tvng@users.noreply.github.com>
2018-04-07 11:51:24 +0200

    Graphique login
    partie graphique du login DONE, il manque le reste : rentrer password etc, avec listeners et verification que ca soit des mdp corrects

--- src/hopital_java/LogIn.java
+++ src/hopital_java/LogIn.java
@@ -9,9 +9,13 @@ import java.awt.*;
 import javax.swing.*;
 
 /**
- *
- * @author Tao Tuong Vi
- */
+ * @author DUCRET Amandine, PAGES Hermance, TAO Tuong Vi
+*/
+
+
+//notes : Le GridBagLayout c'est un truc graphique pour definir l'emplacement de nos objets sur la page
+// et c'est l'enfer
+
 public class LogIn {
     
     //attributs
@@ -40,57 +44,68 @@ public class LogIn {
         
         //pour créer un layout, une structure sur le panel 
         GridBagConstraints grid=new GridBagConstraints();    //c'est léquivalent d'un tab en html
-	
-        grid.fill = GridBagConstraints.VERTICAL;
-        grid.insets = new Insets(5, 5, 5, 5);
-                
-	//grid.gridwidth = 2; // weightx = nombre de cases en abscisse 
-	//grid.gridheight = 6; // weightx = le nombre de cases en ordonnée
-                
+        
+        Dimension dimension = new Dimension(200, 40); //pour la taille des labels et machins
+        
         //texte simple
         JLabel hello = new JLabel("Bienvenue, veuillez vous connecter");
         grid.gridx = 0; //on se positionne sur le grid en (0, 0)
         grid.gridy = 0;
         grid.gridwidth = 2; //le bouton prend la place de 3 boutons en largeur
+        
+        grid.insets = new Insets(0, 0, 50, 0); //marges : top left bot right
         pan.add(hello, grid); //on ajoute le texte en (0,0)
         
         //boutons radio
         local = new JRadioButton("connection locale");
         distant = new JRadioButton("connection distante");
-        ButtonGroup buttgroup = new ButtonGroup();
+        ButtonGroup buttgroup = new ButtonGroup(); //on doit ajouter le tout a un groupe  pour que le radio button soit a choix unique
+        
         buttgroup.add(local);
-        buttgroup.add(distant);
+        buttgroup.add(distant); 
+       
         
-        grid.gridwidth=1;
-        grid.gridy = 1;
+        grid.insets = new Insets(5, 5, 5, 5); //on definit de nouvelles marges pour la suite
+        
+        grid.weightx=1; //on prend 100% de la place
+        grid.anchor = GridBagConstraints.LINE_END;//on aligne le contenu de la case a droite 
+        grid.gridwidth=1; //on reset la valeur comme quoi la cellule du grid prend une seule cellule
+        grid.gridy = 1; //on se positionne sur une nouvelle case (x, y)
         grid.gridx = 0;
-        pan.add(local, grid);
+        pan.add(local, grid); //on ajoute notre bouton local dans cette case
         
+        grid.anchor = GridBagConstraints.LINE_START; //on aligne la case a gauche
         grid.gridy=1;
         grid.gridx =1;
         pan.add(distant, grid);
-        
+       
+ 
         //partie avec rentree d'informations
         lab_nameBDD=new JLabel("Nom de la BDD :");
         lab_login=new JLabel("ID :");
         lab_password=new JLabel("Mot de passe :");
-        
+        //instanciation des zones de txt
         nameBDD=new JTextField();
         login=new JTextField();
         password=new JTextField();  
-        
-        nameBDD.setPreferredSize(new Dimension(200, 50));
-        login.setPreferredSize(new Dimension(200, 50));
-        password.setPreferredSize(new Dimension(200, 50));
+        //on dimensionne
+        nameBDD.setPreferredSize(dimension);
+        login.setPreferredSize(dimension);
+        password.setPreferredSize(dimension);
 
         grid.gridx = 0;
         grid.gridy = 2;
+        grid.anchor = GridBagConstraints.LINE_END;//on aligne a droite
+        
+        //on ajoute les labels
         pan.add(lab_nameBDD, grid);
         grid.gridy = 3;
         pan.add(lab_login, grid);
         grid.gridy = 4;
         pan.add(lab_password, grid);
         
+        //on ajoute les zones de text
+        grid.anchor = GridBagConstraints.LINE_START;
         grid.gridx = 1;
         grid.gridy = 2;
         pan.add(nameBDD, grid);
@@ -99,10 +114,11 @@ public class LogIn {
         grid.gridy = 4;
         pan.add(password, grid);
         
+        //pareil pour le bouton valider
         submit=new JButton("valider");
-         grid.gridy = 5;
-         grid.weightx=2;
+        grid.gridy = 5;
         pan.add(submit, grid);
+        
         
     }
     
