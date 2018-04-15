fea8893e986c20254cf6713a864231946a2cbf0f
Amandine01 <amandine.ducruet@free.fr>
2018-04-15 11:16:33 +0200

    Revert "Test"
    This reverts commit 9324f440c237e8d4095db1e10c837dbb711a80b3.

--- build/classes/hopital_java/Graphic.java
+++ build/classes/hopital_java/Graphic.java
@@ -9,16 +9,16 @@
 package hopital_java;
 
 
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
 /**
  * @author DUCRET Amandine, PAGES Hermance, TAO Tuong Vi
 */
 
 
-public class Graphic extends JFrame {
+public class Graphic extends JFrame{
     
     private CardLayout cl; //pour gerer le changement de panel
     
@@ -35,27 +35,19 @@ public class Graphic extends JFrame {
      
         //different pannels for each functions
        
-        Menu gmenu= new Menu(); //on instancie un pannel pour le menu
-        pan_menu=gmenu.getPan_menu();
-        
+        pan_menu= new JPanel(); //on instancie un pannel pour le menu
+       
         LogIn co=new LogIn();
         pan_logIn=co.getPan_connexion(); // pareil pour le labyrinthe
-        
         getContentPane().add(pan_logIn); // ajouter le panneau dans la fenêtre
-        co.getSubmit().addActionListener(new ListenerChangePanel());
-        
-    }
-    
-    
-       //Ecouteur de ton bouton
-    public class ListenerChangePanel implements ActionListener{
-        public void actionPerformed(ActionEvent clic) {
-            //Appelle la méthode de changement de panel
-           
-            Graphic.this.remove(pan_logIn); //on enleve le pan precedent
-            Graphic.this.getContentPane().add(pan_menu);
-            Graphic.this.revalidate();  //pou reafficher
+        System.out.println("aaaaaaaaaaaa");
+        if (co.salut==true)
+        {   System.out.println("la chenille part tjrs a l'heure");
+            Menu men=new Menu();
+            pan_menu=men.getPan_menu();
+            getContentPane().add(pan_menu);
         }
+        
     }
     
 }
