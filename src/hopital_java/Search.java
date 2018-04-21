be7a43e351d87e52ada37bd8a32beffad1502f95
tvng <37909639+tvng@users.noreply.github.com>
2018-04-21 14:44:53 +0200

    graphique search
    manque service et chambre

--- src/hopital_java/Search.java
+++ src/hopital_java/Search.java
@@ -35,47 +35,47 @@ public class Search extends JPanel {
     private ControlerModule ctrl;
     
     private JPanel panelinfo;
+    private JPanel pi2; //panel dans le panel d'info A=Afficher R=Rechercher
     private JPanel panelsearch;
-    private JComboBox cboTable;
-    private JComboBox cboId;
-    private JComboBox cboId2;
-    private JComboBox cboId3;
-    private JComboBox cboId4;
-    private JComboBox cboId5;
-    private JComboBox cboId6;
-    private JComboBox cboId7;
-    private JComboBox cboId8;
-    //private Controler controler;
     
-    private JCheckBox employe;
-    private JCheckBox docteur;
-    private JCheckBox soigne;
-    private JCheckBox hospitalisation;
-    private JCheckBox malade;
-    private JCheckBox infirmier;
-    private JCheckBox chambre;
-    private JCheckBox service;
     
-    private JCheckBox numero, nom, prenom, adresse, tel;
-    private JCheckBox numero_doc, specialite;
-    private JCheckBox code, nom_service, batiment, directeur;
-    private JCheckBox numero_inf, code_service, rotation, salaire;
-    private JCheckBox code_service_ch, no_ch, nb_lits;
-    private JCheckBox no_malade, nom_malade, prenom_malade, adresse_malade, tel_malade, mutuelle; //malade
-    private JCheckBox no_malade_hosp, code_service_hosp, no_chambre_hosp, lit_hosp; //hospitalisation
-    private JCheckBox no_docteur_soigne, no_malade_soigne;
+    private GridBagConstraints grid;
     
+    private JComboBox combotable; //j'utilise celui la pour connaitre quelle table on selectionne
+    //(= ds quelle table on fait notre recherche bdd)
+    
+    //selectionner les categories que l'on veut afficher
+    private JCheckBox nom, prenom, adresse, tel; //Docteur, Infirmier, Malade 
+    private JCheckBox numero; //Docteur, Infirmier, Malade
+    private JCheckBox mutuelle, nb_soignants; //malade
+    private JCheckBox specialite, nb_soignes; //Docteur
+    private JCheckBox code_service, rotation, salaire; //infirmier
+    
+    private JCheckBox nom_service, batiment, directeur; //service
+    private JCheckBox code_service_ch, no_ch, nb_lits; //chambre
+    
+    
+    //pour faire des recherches 
+    private JTextField tf_nom, tf_prenom;  //Docteur, Infirmier, Malade 
+    private JTextField tf_mutuelle, tf_nb_soignants;//malade
+    private JTextField tf_nb_soignes; //docteur
+    private JComboBox cb_rotation; //infirmier : blank, JOUR, NUIT
+    
+    
+    //+ moyenne salaire et order by alphabetique machin, groupby  *******************************************
+    //a faire plus tard!
+
+   
     private JLabel lab_search;
     private JLabel nameTable;
     private JLabel lab_fonction;
     
-    private JTextField fonction;
+ //   private JTextField fonction;
     
-    private JTextArea textArea;
+   // private JTextArea textArea;
     //private List<String> names = Arrays.asList("employe", "docteur", "soigne", "hospitalisation", "malade", "infirmier", "chambre", "service");
     
     
-    //private JButton submit;
     
     /** CTOR  @param co */
     public Search(Connexion co)
@@ -83,23 +83,127 @@ public class Search extends JPanel {
         co_bdd=co; //on a la base de donnee
         ctrl=new ControlerModule(this);
      
-        //submit_search=new JButton("Lancer la recherche");
-        //submit_search.addActionListener(ctrl);
+        //layout BoxLayout
+        //le "PAGE_AXIS" permet d'afficher tout a la suite verticalement
+        //pour tout afficher horizontalement c'est LINE_AXIS
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+       
+ 
      
-        searchGrid();
+        //Rectangle Informations
+        panelinfo = new JPanel();
+        panelinfo.setLayout(new BoxLayout(panelinfo, BoxLayout.PAGE_AXIS));
+        panelinfo.setBorder(new TitledBorder(null, "Information", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+        //   panelinfo.setBounds(10, 10, 760, 1000);    
+        panelinfo.setPreferredSize(new Dimension(this.getWidth(), 200));
+        
+          //********************************************* dans le rectange d'informations
+        nameTable=new JLabel("Vous recherchez une information dans la table : ");
+        nameTable.setBounds(30, 20, 200, 20);
+        panelinfo.add(nameTable);
+
+        // On cree un tableau de string et on le met dans une ComboBox
+        //ceci est notre combo box pour definir dans quelle table on va faire notre recherche
+        //on limite a 5 tables car les autres sont pas importantes (pour l'instant en tout cas)
+        String[] table = {"Malade","Docteur","Infirmier","Service", "Chambre"};
+        combotable = new JComboBox<>(table);
+        combotable.addItemListener(ctrl);
+        panelinfo.add(combotable);
+        
+        pi2=new JPanel(new GridBagLayout()); 
+        grid=new GridBagConstraints(); 
+        
+        //****************************************************  Rectangle search
+        panelsearch = new JPanel();
+        panelsearch.setBorder( new TitledBorder(null, "Search", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+        panelsearch.setPreferredSize(new Dimension(this.getWidth(), 200));
+        
+        
+        // *********** Declaration des checkbox textfield machin pour le rectangle d'informations
+        //aucun affichage ici!
+       
+        nom= new JCheckBox("nom", false);//doc, inf, mal
+        prenom= new JCheckBox("prenom", false);//doc, inf, mal
+        adresse= new JCheckBox("adresse", false);//doc, inf, mal
+        tel= new JCheckBox("tel", false); //doc, inf, mal
+        numero= new JCheckBox("numero", false); //doc, inf, mal - n°identification
+    
+        mutuelle= new JCheckBox("mutuelle", false);
+        nb_soignants= new JCheckBox("nb de soignants", false); //malade
+    
+        specialite= new JCheckBox("specialite", false);  //doc
+        nb_soignes= new JCheckBox("nb de bsoignes", false);  //Docteur
+        
+        code_service= new JCheckBox("code service", false); 
+        rotation= new JCheckBox("rotation", false);
+        salaire= new JCheckBox("salaire", false);  //infirmier
+    
+        nom_service= new JCheckBox("nom du service", false);
+        batiment= new JCheckBox("batiment", false); 
+        directeur= new JCheckBox("directeur", false);  //service
+        
+        code_service_ch= new JCheckBox("code service", false); 
+        no_ch= new JCheckBox("numero de chambre", false); 
+        nb_lits= new JCheckBox("nombre de lits", false);  //chambre
+    
+    
+    //pour faire des recherches 
+    
+        Dimension d = new Dimension(150, 30); 
+    
+        tf_nom=new JTextField(); //Docteur, Infirmier, Malade 
+        tf_prenom=new JTextField();//Docteur, Infirmier, Malade         
+        tf_mutuelle=new JTextField();//malade
+        tf_nb_soignants=new JTextField();//malade
+        tf_nb_soignes=new JTextField();//doc
+        
+        tf_nom.setPreferredSize(d);
+        tf_prenom.setPreferredSize(d);
+        tf_mutuelle.setPreferredSize(d);
+        tf_nb_soignants.setPreferredSize(d);
+        tf_nb_soignes.setPreferredSize(d);
+        
+        
+        String[] t = {"","JOUR", "NUIT"};
+        cb_rotation = new JComboBox<>(t); //infirmier
+        
+        
+        
+        
+        //*******
+        this.add(panelinfo);
+        this.add(panelsearch);
+        
+          
+        submit_search=new JButton("Lancer la recherche");
+        submit_search.addActionListener(ctrl);
+        
+        
+        //******* affichage basique
+        
+        panelinfo.add(pi2);
+        panelinfo.add(submit_search);
+
+       
+      
+        changeTable("Malade"); //on affiche la table "malade" par defaut
+        
+        this.setVisible(true);
         
     }
     
-    /** */
-
     
     /** */
     public void request()
     {
+        
         System.out.println("REquEST (debug a effacer)");
         String table="";
         String selection="";
-        if(employe.isSelected()){
+        
+     /*   if(employe.isSelected()){
+            
+            System.out.println("eeeeeeeeeeee");
             table=employe.getText();
             
             if(numero.isSelected()){selection=numero.getText();}
@@ -186,71 +290,140 @@ public class Search extends JPanel {
         this.revalidate();
         this.repaint();
         System.out.println(al);
+        
+        */
+    }
+    
+    public void changeTable(String _table)
+    {
+        grid.insets = new Insets(3, 10, 3, 10);
+        
+        pi2.removeAll(); //on efface tout
+      
+        grid.gridwidth=1; //on reset la valeur comme quoi la cellule du grid prend une seule cellule
+        
+        grid.gridy = 0; //on se positionne sur une nouvelle case (x, y)
+        grid.gridx = 0;
+        pi2.add(new JLabel("Cocher pour afficher :"));
+        grid.gridx=1;
+        grid.anchor = GridBagConstraints.LINE_START;
+        pi2.add(new JLabel("   Dont la valeur est :"));
+        
+        
+        if (_table=="Malade" || _table=="Docteur" ||_table=="Infirmier" )
+        {  
+            System.out.println("mdi");
+            //affichage commun des nom, prenom, adresse, telephone
+            grid.anchor = GridBagConstraints.LINE_END;
+            
+            grid.gridy = 1; 
+            grid.gridx = 0;
+            pi2.add(numero, grid);
+            
+            grid.gridy = 2; 
+            grid.gridx = 0;
+            pi2.add(nom, grid);
+            grid.gridx = 1;
+            pi2.add(tf_nom, grid);
+            
+            grid.gridy = 3;
+            grid.gridx = 0;
+            pi2.add(prenom, grid);
+            grid.gridx = 1;
+            pi2.add(tf_prenom, grid);
+            
+            grid.gridy = 4;
+            grid.gridx = 0;
+            pi2.add(adresse, grid);
+            grid.gridy = 5;
+            pi2.add(tel, grid);
+
+            if (_table=="Malade")
+            {  
+                System.out.println(".m");
+                
+                grid.gridy = 6; 
+                grid.gridx = 0;
+                pi2.add(nb_soignants, grid);
+                grid.gridx = 1;
+                pi2.add(tf_nb_soignants, grid);
+               
+                grid.gridy = 7; 
+                grid.gridx = 0;
+                pi2.add(mutuelle, grid);
+                grid.gridx = 1;
+                pi2.add(tf_mutuelle, grid);
+                
+            }
+            else if (_table=="Docteur")
+            {
+                
+                System.out.println(".d");
+                grid.gridx = 0;
+                grid.gridy = 6; 
+                pi2.add(specialite, grid);
+                
+                grid.gridy = 7; 
+                grid.gridx = 0;
+                pi2.add(nb_soignes, grid);
+                grid.gridx = 1;
+                pi2.add(tf_nb_soignes, grid);
+            }
+            else if(_table=="Infirmier")
+            {
+              
+                System.out.println(".i");
+                grid.gridx = 0;
+                grid.gridy = 6; 
+                pi2.add(salaire, grid);
+                grid.gridy = 7; 
+                pi2.add(rotation, grid);
+                grid.gridx = 1;
+                pi2.add(cb_rotation, grid);
+                
+            }
+           
+        }
+        
+        
+        pi2.repaint();
+        pi2.revalidate();
+        
     }
     
     public void searchGrid(){
         
-        //layout BoxLayout
-        //le "PAGE_AXIS" permet d'afficher tout a la suite verticalement
-        //pour tout afficher horizontalement c'est LINE_AXIS
-        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-        
-        panelinfo = new JPanel();
-        panelinfo.setBorder(new TitledBorder(null, "information", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-        panelinfo.setBounds(10, 10, 760, 1000);    
-        panelinfo.setPreferredSize(new Dimension(this.getWidth(), 200));
-        
-        this.add(panelinfo);
-        
-        panelsearch = new JPanel();
-        
-        panelsearch.setBorder( new TitledBorder(null, "Search", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-        panelsearch.setBounds(10, 450, 760, 200); 
-        panelsearch.setPreferredSize(new Dimension(this.getWidth(), 200));
-        this.add(panelsearch);
  
-        /*JButton button=new JButton();
-        textArea = new JTextArea("Taaaa " );
+        
+        
+        /* JButton button=new JButton();
+        textArea = new JTextArea("Taaaa");
         button.setBounds(250, 342, 90, 30);
-        this.add(button);*/
+        this.add(button); */
         
+        /*
         
-        nameTable=new JLabel("Vous recherchez une information dans la table : ");
-        nameTable.setBounds(30, 20, 200, 20);
-        panelinfo.add(nameTable);
-        
-        // On cree un tableau de string et on le met dans une ComboBox
-        String[] table = {"employe","docteur","soigne","hospitalisation","malade","infirmier","chambre","service"};
-        
-        JComboBox<String> combotable = new JComboBox<>(table);
-        combotable.setBounds(250, 20, 100, 20);
-        panelinfo.add(combotable);
-        
-        // Recuperer les infos
-        String selectedTable = (String) combotable.getSelectedItem();
-        System.out.println("Vous avez selectionne la table : " + selectedTable);
-
         lab_search=new JLabel("Les identifiants : ");
         lab_search.setBounds(30, 60, 200, 20);
         panelinfo.add(lab_search);
         
         Vector v2=new Vector();
       
-            numero= new JCheckBox("numero", false);
-            nom= new JCheckBox("nom", false);
-            prenom= new JCheckBox("prenom", false);
-            adresse= new JCheckBox("adresse", false);
-            tel= new JCheckBox("telephone", false);
-            v2.add("Employe");
-            v2.add(numero);
-            v2.add(nom);
-            v2.add(prenom);
-            v2.add(adresse);
-            v2.add(tel);
+        numero= new JCheckBox("numero", false);
+        nom= new JCheckBox("nom", false);
+        prenom= new JCheckBox("prenom", false);
+        adresse= new JCheckBox("adresse", false);
+        tel= new JCheckBox("telephone", false);
+        v2.add("Employe");
+        v2.add(numero);
+        v2.add(nom);
+        v2.add(prenom);
+        v2.add(adresse);
+        v2.add(tel);
       
         
         cboId = new JComboCheckBox(v2);
-        cboId.setBounds(250, 60, 150, 20);
+    //  cboId.setBounds(250, 60, 150, 20);
         panelinfo.add(cboId);
        
         
@@ -262,14 +435,14 @@ public class Search extends JPanel {
         Vector vbis=new Vector();
       
         numero_doc= new JCheckBox("numero_docteur", false);
-            specialite= new JCheckBox("specialite", false);
-            vbis.add("Docteur");
-            vbis.add(numero_doc);
-            vbis.add(specialite);
+        specialite= new JCheckBox("specialite", false);
+        vbis.add("Docteur");
+        vbis.add(numero_doc);
+        vbis.add(specialite);
       
         
         cboId2 = new JComboCheckBox(vbis);
-        cboId2.setBounds(250, 90, 150, 20);
+  //    cboId2.setBounds(250, 90, 150, 20);
         panelinfo.add(cboId2);
         
         
@@ -287,7 +460,7 @@ public class Search extends JPanel {
       
         
         cboId3 = new JComboCheckBox(v3);
-        cboId3.setBounds(250, 120, 150, 20);
+    //  cboId3.setBounds(250, 120, 150, 20);
         panelinfo.add(cboId3);
         
         Vector v4=new Vector();
@@ -304,7 +477,7 @@ public class Search extends JPanel {
       
         
         cboId4 = new JComboCheckBox(v4);
-        cboId4.setBounds(250, 150, 150, 20);
+      //cboId4.setBounds(250, 150, 150, 20);
         panelinfo.add(cboId4);
         
         Vector v5=new Vector();
@@ -319,7 +492,7 @@ public class Search extends JPanel {
       
         
         cboId5 = new JComboCheckBox(v5);
-        cboId5.setBounds(250, 180, 150, 20);
+     // cboId5.setBounds(250, 180, 150, 20);
         panelinfo.add(cboId5);
         
         Vector v6=new Vector();
@@ -338,7 +511,7 @@ public class Search extends JPanel {
       
         
         cboId6 = new JComboCheckBox(v6);
-        cboId6.setBounds(250, 210, 150, 20);
+     // cboId6.setBounds(250, 210, 150, 20);
         panelinfo.add(cboId6);
         
         Vector v7=new Vector();
@@ -355,7 +528,7 @@ public class Search extends JPanel {
       
         
         cboId7 = new JComboCheckBox(v7);
-        cboId7.setBounds(250, 240, 150, 20);
+    //    cboId7.setBounds(250, 240, 150, 20);
         panelinfo.add(cboId7);
         
         Vector v8=new Vector();
@@ -367,28 +540,27 @@ public class Search extends JPanel {
         v8.add(no_malade_soigne);
         
         cboId8 = new JComboCheckBox(v8);
-        cboId8.setBounds(250, 270, 150, 20);
+    //    cboId8.setBounds(250, 270, 150, 20);
         panelinfo.add(cboId8);
                
         lab_fonction=new JLabel("En fonction de : ");
-        lab_fonction.setBounds(30, 300, 200, 20);
+       // lab_fonction.setBounds(30, 300, 200, 20);
         panelinfo.add(lab_fonction);
         
         fonction= new JTextField();
-        fonction.setBounds(250, 300, 150, 20);
+      //  fonction.setBounds(250, 300, 150, 20);
+        fonction.setPreferredSize(new Dimension (100, 20));
         panelinfo.add(fonction);
         
-        submit_search=new JButton("Valider");
-        submit_search.setBounds(250, 342, 90, 30);
-        submit_search.addActionListener(ctrl);
-        this.add(submit_search);
-        
-        textArea = new JTextArea(" ");
-        textArea.setBounds(30, 500, 500, 30);
-        this.add(textArea);
-
-        this.setVisible(true);
+      */
+    }
+    
+    public void change_box()
+    {
         
     }
+    
+  
+    
     
 }
